388994b62d9ae46f828ff423459f16ae73a14788
muhdfasil <muhammedfasiltk32745@gmail.com>
2025-03-21 05:03:54 +0530

    update

---
+++ pgm 6/LinearSearch.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+
+public class LinearSearch{
+    public static void main(String[] args) {
+        int i,n,search,array[];
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter number of elements");
+        n=s.nextInt();
+        array=new int[n];
+        System.out.println("Enter Elements");
+        for(i=0;i<n;i++){
+            array[i]=s.nextInt();
+            System.out.println("Enter element to search");
+            search=s.nextInt();
+            for(i=0;i<n;i++){
+                if(array[i]==search){
+                    System.out.println("found in "+(i+1)+"th location");
+                }
+                else{
+                    System.out.println("Not found");
+                }
+            }
+        }
+    }
+}
